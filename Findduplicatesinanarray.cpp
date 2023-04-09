e4c6c07a580c3ee6ae1efafca6da6fcf93fa7dc7
Chetanya Kumar <114152088+chetanyachauhan3010@users.noreply.github.com>
2023-04-09 18:11:13 +0530

    Create Findduplicatesinanarray.cpp

---
+++ Findduplicatesinanarray.cpp
@@ -0,0 +1,40 @@
+ vector<int> duplicates(int arr[], int n) {
+        for (int i = 0; i < n; i++) {
+        arr[i] += 1;
+    }
+  
+    vector<int> res;
+  
+    int count = 0;
+  
+    for (int i = 0; i < n; i++) {
+  
+        int index = abs(arr[i]) > n ? abs(arr[i]) / (n + 1)
+                                    : abs(arr[i]);
+  
+        if (index == n) {
+            count++;
+            continue;
+        }
+          int val = arr[index];
+  
+        if (val < 0) {
+            res.push_back(index - 1);
+            arr[index] = abs(arr[index]) * (n + 1);
+        }
+        else if (val > n)
+            continue;
+        else
+            arr[index] = -arr[index];
+    }
+  
+    if (count > 1)
+        res.push_back(n - 1);
+  
+    if (res.size() == 0)
+        res.push_back(-1);
+    else
+        sort(res.begin(), res.end());
+  
+    return res;
+    }
