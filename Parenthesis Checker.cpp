a385fd3295ea37682c6e58d0eef8438a0965a0f6
Chetanya Kumar <114152088+chetanyachauhan3010@users.noreply.github.com>
2023-04-16 21:27:36 +0530

    Create Parenthesis Checker.cpp

---
+++ Parenthesis Checker.cpp
@@ -0,0 +1,16 @@
+bool ispar(string s)
+    {
+        // Your code here
+        int i=-1;
+        for(auto& ch:s){
+            if(ch=='(' || ch=='{' || ch=='[')
+                s[++i]=ch;
+            else{
+                if(i>=0 && ((s[i]=='(' && ch==')') || (s[i]=='{' && ch=='}') || (s[i]=='[' && ch==']')))
+                    i--;
+                else
+                    return false;
+            }
+        }
+        return i==-1;
+    }
